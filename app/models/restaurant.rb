313a711a1e82855d56e326d8215df695b43996da
lucie galichon <lgalichon@gmail.com>
2015-10-28 14:24:44 +0100

    validations added to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+
+
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false }
+  validates :name, presence: true
+  validates :address, presence: true
 end
